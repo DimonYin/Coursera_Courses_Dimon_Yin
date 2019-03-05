03222dea3585cbbbb096317cad933f23c7fbb689
Dimon Yin <dimonyin@gmail.com>
2019-03-06 04:05:12 +1100

    ..

--- Principles of Computing (Part 1)/Homework1.py
+++ Principles of Computing (Part 1)/Homework1.py
@@ -1,1 +1,8 @@
-print ("hello")
+def appendsums(lst):
+    """
+    Repeatedly append the sum of the current last three elements
+    of lst to lst.
+    """
+    for x in range (0,24):
+        sum = lst[-1] + lst[-2] + lst[-3]
+        lst.append(sum)
